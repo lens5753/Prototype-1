68f6b45a07ea412a7ea07c42e48f82fa50972c3b
lens5753 <56047006+lens5753@users.noreply.github.com>
2019-11-05 12:29:19 -0800

    Added Player control

--- Assets/Scripts/FollowPlayer.cs
+++ Assets/Scripts/FollowPlayer.cs
@@ -6,6 +6,7 @@ public class FollowPlayer : MonoBehaviour
 {
     public GameObject player;
     private Vector3 offset = new Vector3(0, 7, -10);
+    
     // Start is called before the first frame update
     void Start()
     {

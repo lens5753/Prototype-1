5a0267410a7ac24d3838134ce7b1a2d671db6628
lens5753 <56047006+lens5753@users.noreply.github.com>
2019-11-14 12:11:25 -0800

    Propeller spins

--- Assets/Challenge 1/Scripts/SpinPropellerX.cs
+++ Assets/Challenge 1/Scripts/SpinPropellerX.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class SpinPropellerX : MonoBehaviour
 {
+    public float rotationSpeed;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +15,6 @@ public class SpinPropellerX : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        transform.Rotate(Vector3.back * rotationSpeed);
     }
 }
